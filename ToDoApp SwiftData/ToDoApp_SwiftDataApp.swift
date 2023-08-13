017b260f8fb3d012ebf8f6f440f83faf6dc8516b
Yauheni Levin <evgenylevin91@gmail.com>
2023-08-13 17:18:59 +0200

    Added Model and Query (#1)

--- ToDoApp SwiftData/ToDoApp_SwiftDataApp.swift
+++ ToDoApp SwiftData/ToDoApp_SwiftDataApp.swift
@@ -12,6 +12,7 @@ struct ToDoApp_SwiftDataApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: ToDoItem.self)
         }
     }
 }
