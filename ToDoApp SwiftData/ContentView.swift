27705c29873931f46c3a229d28de104c67ec3b7e
Evgeny Levin <evgenylevin91@gmail.com>
2023-08-13 12:47:38 +0200

    Initial Commit

---
+++ ToDoApp SwiftData/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ToDoApp SwiftData
+//
+//  Created by YAUHENI LEVIN on 13.08.23.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoApp SwiftData/ToDoApp_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_SwiftDataApp.swift
+//  ToDoApp SwiftData
+//
+//  Created by YAUHENI LEVIN on 13.08.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
